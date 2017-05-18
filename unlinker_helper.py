d727dd2f8ee4e14ae052ba41ed6ee14d57c8a063
Toms <tomsons26lv@gmail.com>
2017-05-18 20:58:53 +0300

    Add Unlinker Helper python version
    Reason for this is file offset in IDC version is failing in various binaries returning a value offseted by 0xC00 or similar

---
+++ unlinker_helper.py
@@ -0,0 +1,34 @@
+import idaapi
+
+#Unlinker Helper script
+#by tomsons26
+#Prints basic info needed for the Unlinker INI's to IDA Output Window
+
+#PROBABLY_TODO
+#Make it segment sensetive,
+#SegName(ea) returns
+#if .text use function format,
+#if .rdata use rdata format,
+#if .data use data format.
+
+#Get current cursor location
+cursor = ScreenEA()
+
+#Get function name at current cursor location
+function_name = GetFunctionName(cursor);
+
+#Get current Function start address
+function_address = GetFchunkAttr(cursor,FUNCATTR_START);
+
+#Get current function file address
+file_address = idaapi.get_fileregion_offset(function_address);
+
+#Get current function size
+function_size = GetFchunkAttr(cursor,FUNCATTR_END)-GetFchunkAttr(cursor,FUNCATTR_START);
+
+
+#Print gathered info to output
+Message("[%s]\n" % function_name);
+Message("Address=%Xh\n" % function_address);
+Message("FileAddress=%Xh\n" % file_address);
+Message("Size=%Xh\n" % function_size);	
